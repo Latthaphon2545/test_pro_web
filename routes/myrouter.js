ff91d4b3d77bd4033866f363e784b8fd0b7641eb
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2023-10-16 00:18:37 +0700

    up

--- routes/myrouter.js
+++ routes/myrouter.js
@@ -1,19 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
-// use mongoose
-const Product = require('../models/products');
-
-
-router.get('/take_or_dine', (req, res) => {
-    res.render('take_or_dine.ejs',{products: products});
+router.get('/menu', (req, res) => {
+    const products = [
+        {
+          id: 1,
+          img: 'images/Thai-Omelette-HP-191008.jpg',
+          name: 'Omelet',
+          price: 50,
+          type: 'fry',
+          AddOn: ['Egg', 'Onion', 'Tomato', 'Chili', 'Salt', 'Pepper', 'Rice']
+        },
+        {
+          id: 2,
+          img: 'images/fried-chicken.jpg',
+          name: 'Fried Chicken',
+          price: 60,
+          type: 'fry',
+          AddOn: ['Chicken', 'Salt', 'Pepper', 'Oil']
+        },
+        {
+          id: 3,
+          img: 'images/stir-fry-with-curry.jpg',
+          name: 'Stir fry with curry',
+          price: 70,
+          type: 'stir-fry',
+          AddOn: ['Chicken', 'Salt', 'Pepper', 'Oil', 'Curry']
+        }
+      ];
+    res.render('menu', {products: products});
 });
 
-router.get('/menu', (req, res) => {
-    Product.find().exec((err, doc) => {
-        if(err) throw err;
-        res.render('menu.ejs',{products: doc});
-    });
+router.get('/Restaurant_done', (req, res) => {
+    res.render('POS_done');
+});
+
+router.get('/Restaurant_pay', (req, res) => {
+  res.render('POS_pay');
+});
+
+router.get('/take_or_dine', (req, res) => {
+    res.render('take_or_dine');
+});
+
+router.get('/receipt', (req, res) => {
+    res.render('receipt');
 });
 
 const path = require('path');
