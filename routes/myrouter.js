341973a88c58e639c2395ff4c11ad5297433be21
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2023-10-21 17:47:28 +0700

    up

--- routes/myrouter.js
+++ routes/myrouter.js
@@ -2,33 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/menu', (req, res) => {
-    const products = [
-        {
-          id: 1,
-          img: 'images/Thai-Omelette-HP-191008.jpg',
-          name: 'Omelet',
-          price: 50,
-          type: 'fry',
-          AddOn: ['Egg', 'Onion', 'Tomato', 'Chili', 'Salt', 'Pepper', 'Rice']
-        },
-        {
-          id: 2,
-          img: 'images/fried-chicken.jpg',
-          name: 'Fried Chicken',
-          price: 60,
-          type: 'fry',
-          AddOn: ['Chicken', 'Salt', 'Pepper', 'Oil']
-        },
-        {
-          id: 3,
-          img: 'images/stir-fry-with-curry.jpg',
-          name: 'Stir fry with curry',
-          price: 70,
-          type: 'stir-fry',
-          AddOn: ['Chicken', 'Salt', 'Pepper', 'Oil', 'Curry']
-        }
-      ];
-    res.render('menu', {products: products});
+    res.render('menu');
 });
 
 router.get('/Restaurant_done', (req, res) => {
